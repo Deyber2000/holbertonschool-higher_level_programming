f1860dd6add7c2c03c334a91e8782ce14a324bb8
Deyber2000 <garzondeiber6@gmail.com>
2020-09-01 13:26:21 -0500

    fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -2):
-    print(chr(i), end="")
-    print(chr(i - 33), end="")
+for i in reversed(range(ord('a'), ord('z') + 1)):
+    if (i % 2 == 1):
+        i -= 32
+    print("{:c}".format(i), end="")
