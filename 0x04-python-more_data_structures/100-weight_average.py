55abd0974361b96b001a5f3527b1fd691a3fd50c
Deyber2000 <garzondeiber6@gmail.com>
2020-09-10 12:16:18 -0500

    fixed

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+    avg = 0
+    weight = 0
+    for x in my_list:
+        avg += x[0] * x[1]
+        weight += x[1]
+    return (avg / weight)
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom_num = {'I': 1,
+               'V': 5,
+               'X': 10,
+               'L': 50,
+               'C': 100,
+               'D': 500,
+               'M': 1000
+               }
+
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+
+    converted = 0
+    length = len(roman_string)
+
+    for i in range(length):
+        if i is (length - 1):
+            converted += rom_num[roman_string[i]]
+        else:
+            if rom_num[roman_string[i]] >= rom_num[roman_string[i + 1]]:
+                converted += rom_num[roman_string[i]]
+            else:
+                converted -= rom_num[roman_string[i]]
+    return (converted)
