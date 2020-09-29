0e857e540e582ba0757132c83b25a15a216ee5cf
Deyber2000 <garzondeiber6@gmail.com>
2020-09-29 18:18:39 -0500

    using join function

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-return string
-"""
-
-
-def magic_string():
-    for x in range():
-        return("Holberton" if x == len() else "Holberton, ")
+def magic_string(H=[]):
+    H += ["Holberton"]
+    return (", ".join(H))
