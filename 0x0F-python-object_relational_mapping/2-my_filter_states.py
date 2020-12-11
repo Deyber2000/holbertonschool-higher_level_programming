9ad42884b314d5ed2eba2e313a499b92e122f1b5
Deyber2000 <garzondeiber6@gmail.com>
2020-12-11 15:00:28 -0500

    filtering with python

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Lists all states from a database """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    nmrows = cur.execute("SELECT id,name FROM `states`")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+            
