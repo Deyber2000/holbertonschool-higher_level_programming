4b4b9e53d6a9e12cac1e73181f38fa698cd7eef6
Deyber2000 <garzondeiber6@gmail.com>
2021-01-11 13:28:27 -0500

    changing value for email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {'email': sys.argv[2]}
+    email = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode(value)
+    data = urllib.parse.urlencode(email)
     data = data.encode('ascii')
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
