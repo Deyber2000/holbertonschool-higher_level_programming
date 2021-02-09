c1c7aeb4b614a2c5cdafb691cf23164a9721c30c
Deyber2000 <garzondeiber6@gmail.com>
2021-02-09 12:32:24 -0500

    write line

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const writeString = process.argv[3];
+
+fs.writeFile(filePath, writeString, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+});
