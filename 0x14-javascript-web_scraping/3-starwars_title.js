51dded636ab917a374026875e0dec1543d25f72c
Deyber2000 <garzondeiber6@gmail.com>
2021-02-09 13:07:55 -0500

    change format

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-// makes get request for SW movie id
 const request = require('request');
-request('http://swapi.co/api/films/${process.argv[2]}', function (error, response, body) {
-  error && console.log(error);
-  console.log(JSON.parse(body).title);
+let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
