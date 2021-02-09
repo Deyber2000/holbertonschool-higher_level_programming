c54e05962a14f03f05271a0be4cce7a9fa239dd1
Deyber2000 <garzondeiber6@gmail.com>
2021-02-09 12:49:16 -0500

    parameter mode

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-
+// makes get request for SW movie id
 const request = require('request');
-const episode = 'http://swapi.co/api/films/' + process.argv[2];
-
-request(episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const dict = JSON.parse(body);
-    console.log(dict.title);
-  }
+request(`http://swapi.co/api/films/${process.argv[2]}`, function (error, response, body) {
+  error && console.log(error);
+  console.log(JSON.parse(body).title);
 });
